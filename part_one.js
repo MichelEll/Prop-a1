df2b4d54188120d94a991b7687ee1a8669821e93
fabianjohansson <fabain_j@hotmail.com>
2019-11-27 10:36:57 +0100

    started with myObject.create method

---
+++ part_one.js
@@ -0,0 +1,21 @@
+var myObject = {
+    prototypeList: [],
+
+    create: function(listOfParents) {
+    //skapa ny funktion för
+       return {
+            prototypeList : [listOfParents],
+                //prototypeList.push(newObject);
+           __proto__:this,
+             call: function(funkName,parameters) {
+        }
+
+
+
+    }
+
+
+    }
+}
+
+myObject.create()
