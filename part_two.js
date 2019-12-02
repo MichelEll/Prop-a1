04301ea2226f5dfa4949e393b08db0a17df970c0
MichelELL <michel.mottet@hotmail.com>
2019-12-02 11:50:33 +0100

    part two done and done

--- part_two.js
+++ part_two.js
@@ -7,90 +7,87 @@ function createClass(name, superClassList) {
     let myClass = {
         className: name,
         inheritanceList: superClassList,
-        call: function (funkName, parameters) {
-            if (typeof this[funkName] === "function") {
-                return this[funkName](parameters);
+
+
+        addSuperClass: function (obj) {
+            if (!obj.__check(this)) {
+                this.inheritanceList.push(obj);
+                console.log("added!");
+            } else {
+                throw "error, would cause circular inheritence"
             }
-            if (this.inheritanceList.length != null) {
+        },
+        __check: function (obj) {
+            let bool = false;
+            if (this.inheritanceList != null) {
+                if (this.inheritanceList.includes(obj)) {
+                    return true;
+                }
                 for (let i = 0; i < this.inheritanceList.length; i++) {
-                    let result = this.inheritanceList[i].call(funkName, parameters);
-                    if (result != null) {
-                        return result;
-                    }
+                    bool = this.inheritanceList[i].__check(obj);
                 }
             }
-            return null;
+            return bool;
         },
-
+        __find: function (funcName) {
+            if (typeof this[funcName] === "function") {
+                return this;
+            }
+            if (this.inheritanceList != null) {
+                for (let i = 0; i < this.inheritanceList.length; i++) {
+                    let obj = this.inheritanceList[i].__find(funcName);
+                    if (obj != null) {
+                        return obj;
+                    }
+                }
+                return null;
+            }
+        },
         new: function () {
-            return Object.create(this);
+            var obj = Object.create(this);
+
+            obj.call = function (funcName, parameters) {
+                if (typeof this[funcName] === "function") {
+                    return this[funcName](parameters);
+                }
+                if (this.inheritanceList != null) {
+                    for (let i = 0; i < this.inheritanceList.length; i++) {
+                        let obj = this.inheritanceList[i].__find(funcName);
+                        if (obj != null) {
+                            return obj[funcName](parameters);
+                        }
+                    }
+                }
+                return null;
+            }
+            return obj;
         },
     };
     return myClass;
 }
 
 
-
-/*
-createClass = function(className, superClassList){
-    let myClass= {
-
-        name: className,
-        inheritanceList: superClassList,
-
-    }
-    new: function () {
-
-
-    }
-}
-*/
-
-
-/*
-let MyObject = {
-    inheritenceList: [],
-    className,
-
-    createClass: function (className, superClassList) {
-        return {
-            this.className = className,
-            inheritenceList: superClassList,
-    },
-
- */
-/*
-  new: function(){
-
-  }
-  call: function (funcName, parameters) {
-
-
-
-  }
-
-  addSuperClass: function (classParam) {
-
-          return null;
-  }
-
- */
-
-
 // testing code used to test
 
 var class0 = createClass("Class0", null);
 
-class0.func = function(arg) { return "func0: " + arg; };
+class0.func = function (arg) {
+    return "func0: " + arg;
+};
 var class1 = createClass("Class1", [class0]);
 var class2 = createClass("Class2", []);
-class2.func = function(arg) { return "func2: " + arg; };
+class2.func = function (arg) {
+    return "func2: " + arg;
+};
 var class3 = createClass("Class3", [class1, class2]);
 var obj3 = class3.new();
+console.log(obj3);
 var result = obj3.call("func", ["hello"]);
 
 class0 = createClass("Class0", null);
-class0.func = function(arg) { return "func0: " + arg; };
+class0.func = function (arg) {
+    return "func0: " + arg;
+};
 class1 = createClass("Class1", [class0]);
 class2 = createClass("Class2", []);
 class3 = createClass("Class3", [class2, class1]);
@@ -98,11 +95,18 @@ obj3 = class3.new();
 result = obj3.call("func", ["hello"]);
 
 class0 = createClass("Class0", null);
-class0.func = function(arg) { return "func0: " + arg; };
+class0.func = function (arg) {
+    return "func0: " + arg;
+};
 var obj0 = class0.new();
 result = obj0.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
 
 
+
+var class0 = createClass("Class 0", null);
+var class1 = createClass("Class 1", [class0]);
+class0.addSuperClass(class1);
 
 
 
